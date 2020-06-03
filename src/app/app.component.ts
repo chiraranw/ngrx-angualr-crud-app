9046d9ed5db863d0921022bab7ba50cc9b2b3014
Nation Chirara <chiraranw@gmail.com>
2020-06-03 15:33:12 -0700

    initial commit - service working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { StudentService } from './services/student.service';
+import { tap } from 'rxjs/operators';
+import { Student } from './model/student.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'simple-ngrx-app';
+  public students:Student[]=[]
+  constructor(private studentApi:StudentService){
+     this.studentApi.getStudents().subscribe(
+       (res)=>{
+         this.students=res
+         console.log("Data....", res);         
+       }
+     );
+    
+  }
 }
---
+++ src/app/services/student.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { Student } from '../model/student.model';
+import { Injectable } from '@angular/core';
+import {map, catchError} from 'rxjs/operators';
+
+@Injectable()
+export class StudentService {
+
+    public students:Student[]=[];
+    constructor(private http: HttpClient) { }
+
+    public getStudents(): Observable<Student[]> {
+        return this.http.get<Student[]>("http://localhost:3000/students");
+    }
+}
